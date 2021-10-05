cae99f31d5fc25ebf762d44fa97c8c9a1141d132
CodeClan <~}@~=N!1nizD&nxfg#7>
2021-10-05 14:49:48 +0200

    closes #15 closes 16

---
+++ pet-clinic-web/src/main/java/org/rickhuizing/petclinicguru/controllers/VetController.java
@@ -0,0 +1,13 @@
+package org.rickhuizing.petclinicguru.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets"})
+    public String listVets() {
+        return "vets/index";
+    }
+}
