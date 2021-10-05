f9050ba771a1059f2d0ae0dd477c268da47baeb3
CodeClan <~}@~=N!1nizD&nxfg#7>
2021-10-05 16:12:26 +0200

    closes #12

--- pet-clinic-web/src/main/java/org/rickhuizing/petclinicguru/controllers/VetController.java
+++ pet-clinic-web/src/main/java/org/rickhuizing/petclinicguru/controllers/VetController.java
@@ -1,13 +1,22 @@
 package org.rickhuizing.petclinicguru.controllers;
 
+import org.rickhuizing.petclinicguru.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets"})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
