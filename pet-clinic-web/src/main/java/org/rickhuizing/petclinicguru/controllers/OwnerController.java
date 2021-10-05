3de0468dcc6a5f020a44bda30a910ff46f672427
CodeClan <~}@~=N!1nizD&nxfg#7>
2021-10-05 16:09:34 +0200

    closes #13

--- pet-clinic-web/src/main/java/org/rickhuizing/petclinicguru/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/org/rickhuizing/petclinicguru/controllers/OwnerController.java
@@ -1,16 +1,24 @@
 package org.rickhuizing.petclinicguru.controllers;
 
 
+import org.rickhuizing.petclinicguru.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
-    @RequestMapping({"", "/"})
-    public String listOwners() {
+    private final OwnerService ownerService;
 
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+    @RequestMapping({"", "/"})
+    public String listOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/owners";
     }
 }
