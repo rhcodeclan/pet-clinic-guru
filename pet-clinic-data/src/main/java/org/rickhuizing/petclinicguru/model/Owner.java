e4808068905b1bfe0aeb564324c2b553ed9d4fae
CodeClan <~}@~=N!1nizD&nxfg#7>
2021-10-06 12:24:48 +0200

    closes #22)

--- pet-clinic-data/src/main/java/org/rickhuizing/petclinicguru/model/Owner.java
+++ pet-clinic-data/src/main/java/org/rickhuizing/petclinicguru/model/Owner.java
@@ -1,4 +1,14 @@
 package org.rickhuizing.petclinicguru.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
 public class Owner extends Person {
+
+    private Set<Pet> pets = new HashSet<>();
 }
