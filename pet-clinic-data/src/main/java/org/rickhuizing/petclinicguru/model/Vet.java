b1d2d596ceadbb51d8ac2ecbb7dbc3bf64ba3a8d
CodeClan <~}@~=N!1nizD&nxfg#7>
2021-10-06 12:26:39 +0200

    closes #23

--- pet-clinic-data/src/main/java/org/rickhuizing/petclinicguru/model/Vet.java
+++ pet-clinic-data/src/main/java/org/rickhuizing/petclinicguru/model/Vet.java
@@ -1,6 +1,8 @@
 package org.rickhuizing.petclinicguru.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
 
-
+    private Set<VetSpeciality> specialities;
 }
---
+++ pet-clinic-data/src/main/java/org/rickhuizing/petclinicguru/model/VetSpeciality.java
@@ -0,0 +1,10 @@
+package org.rickhuizing.petclinicguru.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class VetSpeciality extends BaseEntity {
+    private String description;
+}
